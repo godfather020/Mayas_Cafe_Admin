90b52dcc1d753a9e82cc39ebf5ddcc11503095b7
Rahul singh Sajwan <sajwanrah020@gmail.com>
2022-07-19 16:45:21 +0530

    added inActive offers API added expired offers API

--- app/src/main/java/com/example/mayas_cafe_admin/fragments/ExpiredOffers_frag.kt
+++ app/src/main/java/com/example/mayas_cafe_admin/fragments/ExpiredOffers_frag.kt
@@ -1,15 +1,25 @@
 package com.example.mayas_cafe_admin.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mayas_cafe_admin.MainActivity
 import com.example.mayas_cafe_admin.R
+import com.example.mayas_cafe_admin.fragments.ViewModel.ActiveOffers_ViewModel
 import com.example.mayas_cafe_admin.recycleModels.recycleModel.RecycleModel
 import com.example.mayas_cafe_admin.recycleModels.recycleViewModels.RecycleView_AO
 import com.example.mayas_cafe_admin.recycleModels.recycleViewModels.RecycleView_O
-import java.util.ArrayList
+import com.example.mayas_cafe_admin.utils.Constants
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ExpiredOffers_frag : Fragment() {
@@ -18,6 +28,21 @@ class ExpiredOffers_frag : Fragment() {
     lateinit var  recyclerView: RecyclerView
     lateinit var recycleView_adapter_O : RecycleView_O
     lateinit var search : MenuItem
+    lateinit var mainActivity: MainActivity
+    lateinit var activeOffersViewModel: ActiveOffers_ViewModel
+    lateinit var loadingExpired : ProgressBar
+    var token : String? = ""
+    lateinit var offersId: ArrayList<String>
+    lateinit var offersName: ArrayList<String>
+    lateinit var offersTitle: ArrayList<String>
+    lateinit var offersDes: ArrayList<String>
+    lateinit var offersCode: ArrayList<String>
+    lateinit var offersCalcType: ArrayList<String>
+    lateinit var offersUpTo: ArrayList<String>
+    lateinit var offersMin: ArrayList<String>
+    lateinit var offersStartAt: ArrayList<String>
+    lateinit var offersStopAt: ArrayList<String>
+    lateinit var offersImg: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +58,163 @@ class ExpiredOffers_frag : Fragment() {
         // Inflate the layout for this fragment
         val view : View = inflater.inflate(R.layout.fragment_expired_offers_frag, container, false)
 
+        activeOffersViewModel = ViewModelProvider(this).get(ActiveOffers_ViewModel::class.java)
+
+        mainActivity = activity as MainActivity
+
         recyclerView= view.findViewById(R.id.expired_offers_rv)
+        loadingExpired = view.findViewById(R.id.loading_expired)
+
+        loadingExpired.visibility = View.VISIBLE
+
         val layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
 
+        offersId = ArrayList<String>()
+        offersName = ArrayList<String>()
+        offersCalcType = ArrayList<String>()
+        offersCode = ArrayList<String>()
+        offersDes = ArrayList<String>()
+        offersTitle = ArrayList<String>()
+        offersMin = ArrayList<String>()
+        offersUpTo = ArrayList<String>()
+        offersStartAt = ArrayList<String>()
+        offersStopAt = ArrayList<String>()
+        offersImg = ArrayList<String>()
+
         setHasOptionsMenu(true)
 
+        token =
+            mainActivity.getSharedPreferences(
+                Constants.sharedPrefrencesConstant.DEVICE_TOKEN,
+                0
+            )
+                .getString(
+                    Constants.sharedPrefrencesConstant.DEVICE_TOKEN, ""
+                )
+
         //setUpExpiredOffersRv()
+
+        getExpiredOffers()
 
         return view
     }
 
-    private fun setUpExpiredOffersRv() {
+    private fun getExpiredOffers() {
+
+        if (token != null){
+
+            activeOffersViewModel.getActiveOffers(this, token.toString(), loadingExpired).observe(viewLifecycleOwner){
+
+                if (it != null){
+
+                    if (it.getSuccess()!!){
+
+                        clearArrayLists()
+
+                        for (i in it.getData()!!.ListcouponResponce!!.indices) {
+
+                            val dateFormat: DateFormat =
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                    SimpleDateFormat(
+                                        "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+                                        Locale.getDefault()
+                                    )
+                                } else {
+                                    TODO("VERSION.SDK_INT < N")
+                                }
+                            val date =
+                                dateFormat.parse(it.getData()!!.ListcouponResponce!![i].stopAt.toString()) //You will get date object relative to server/client timezone wherever it is parsed
+
+                            val date1 =
+                                dateFormat.parse(it.getData()!!.ListcouponResponce!![i].startAt.toString())
+
+                            val formatter: DateFormat =
+                                SimpleDateFormat("dd-MM-yyyy") //If you need time just put specific format for time like 'HH:mm:ss'
+
+                            val dateStr = formatter.format(date)
+
+                            val startDate = formatter.format(date1)
+
+                            val valid_until = dateStr
+                            val sdf = SimpleDateFormat("dd-MM-yyyy")
+                            var strDate: Date? = null
+                            try {
+                                strDate = sdf.parse(valid_until)
+                            } catch (e: ParseException) {
+                                e.printStackTrace()
+                            }
+
+                            if (Date().after(strDate)) {
+
+                                Log.d("time", "stopgreater")
+
+                                offersId.add(it.getData()!!.ListcouponResponce!![i].id.toString())
+                                offersName.add(it.getData()!!.ListcouponResponce!![i].name.toString())
+                                offersDes.add(it.getData()!!.ListcouponResponce!![i].desc.toString())
+                                offersCode.add(it.getData()!!.ListcouponResponce!![i].code.toString())
+                                offersTitle.add(it.getData()!!.ListcouponResponce!![i].title.toString())
+                                offersCalcType.add(it.getData()!!.ListcouponResponce!![i].calculateType.toString())
+                                offersImg.add(it.getData()!!.ListcouponResponce!![i].bannerImage.toString())
+                                offersMin.add(it.getData()!!.ListcouponResponce!![i].minimumAmount.toString())
+                                offersUpTo.add(it.getData()!!.ListcouponResponce!![i].uptoDiscount.toString())
+                                offersStartAt.add(startDate)
+                                offersStopAt.add(dateStr)
+
+                            } else {
+
+                                Log.d("time121", "todaygreater")
+                            }
+                        }
+
+                        loadingExpired.visibility = View.GONE
+                        setUpExpiredOffersRv()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clearArrayLists() {
 
         recycleView_models.clear()
+        offersCode.clear()
+        offersId.clear()
+        offersName.clear()
+        offersMin.clear()
+        offersTitle.clear()
+        offersCalcType.clear()
+        offersDes.clear()
+        offersImg.clear()
+        offersStartAt.clear()
+        offersStopAt.clear()
+        offersUpTo.clear()
+    }
 
-        recycleView_models.add(RecycleModel("40%","sadads", "Porridge", "Flat discount on every purchase", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Sauteed Kale", "For first time user only", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Porridge", "Flat discount on every purchase", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Sauteed Kale", "For first time user only", "Offer available only 21 to 23 April"))
+    private fun setUpExpiredOffersRv() {
 
-        recycleView_adapter_O = RecycleView_O(activity, recycleView_models)
-        recyclerView.adapter = recycleView_adapter_O
-        recycleView_adapter_O.notifyDataSetChanged()
+        for (i in offersId.indices) {
+
+            recycleView_models.add(
+                RecycleModel(
+                    offersId[i],
+                    offersName[i],
+                    offersTitle[i],
+                    offersCode[i],
+                    offersDes[i],
+                    offersCalcType[i],
+                    offersUpTo[i],
+                    offersMin[i],
+                    offersStartAt[i],
+                    offersStopAt[i],
+                    offersImg[i]
+                )
+            )
+
+            recycleView_adapter_O = RecycleView_O(activity, recycleView_models)
+            recyclerView.adapter = recycleView_adapter_O
+            recycleView_adapter_O.notifyItemInserted(i)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- app/src/main/java/com/example/mayas_cafe_admin/fragments/InactiveOffers_frag.kt
+++ app/src/main/java/com/example/mayas_cafe_admin/fragments/InactiveOffers_frag.kt
@@ -1,14 +1,24 @@
 package com.example.mayas_cafe_admin.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mayas_cafe_admin.MainActivity
 import com.example.mayas_cafe_admin.R
+import com.example.mayas_cafe_admin.fragments.ViewModel.ActiveOffers_ViewModel
 import com.example.mayas_cafe_admin.recycleModels.recycleModel.RecycleModel
 import com.example.mayas_cafe_admin.recycleModels.recycleViewModels.RecycleView_O
-import java.util.ArrayList
+import com.example.mayas_cafe_admin.utils.Constants
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class InactiveOffers_frag : Fragment() {
@@ -17,6 +27,21 @@ class InactiveOffers_frag : Fragment() {
     lateinit var  recyclerView: RecyclerView
     lateinit var recycleView_adapter_O : RecycleView_O
     lateinit var search : MenuItem
+    lateinit var activeOffersViewModel: ActiveOffers_ViewModel
+    lateinit var mainActivity: MainActivity
+    lateinit var loadingInactive : ProgressBar
+    var token : String? = ""
+    lateinit var offersId: ArrayList<String>
+    lateinit var offersName: ArrayList<String>
+    lateinit var offersTitle: ArrayList<String>
+    lateinit var offersDes: ArrayList<String>
+    lateinit var offersCode: ArrayList<String>
+    lateinit var offersCalcType: ArrayList<String>
+    lateinit var offersUpTo: ArrayList<String>
+    lateinit var offersMin: ArrayList<String>
+    lateinit var offersStartAt: ArrayList<String>
+    lateinit var offersStopAt: ArrayList<String>
+    lateinit var offersImg: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,29 +57,163 @@ class InactiveOffers_frag : Fragment() {
         // Inflate the layout for this fragment
         val view : View = inflater.inflate(R.layout.fragment_inactive_offers_frag, container, false)
 
+        activeOffersViewModel = ViewModelProvider(this).get(ActiveOffers_ViewModel::class.java)
+        mainActivity = activity as MainActivity
+
         recyclerView= view.findViewById(R.id.inactive_offers_rv)
+        loadingInactive = view.findViewById(R.id.loading_inactive)
+
+        loadingInactive.visibility = View.VISIBLE
+
         val layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
 
+        offersId = ArrayList<String>()
+        offersName = ArrayList<String>()
+        offersCalcType = ArrayList<String>()
+        offersCode = ArrayList<String>()
+        offersDes = ArrayList<String>()
+        offersTitle = ArrayList<String>()
+        offersMin = ArrayList<String>()
+        offersUpTo = ArrayList<String>()
+        offersStartAt = ArrayList<String>()
+        offersStopAt = ArrayList<String>()
+        offersImg = ArrayList<String>()
+
         setHasOptionsMenu(true)
+
+        token =
+            mainActivity.getSharedPreferences(
+                Constants.sharedPrefrencesConstant.DEVICE_TOKEN,
+                0
+            )
+                .getString(
+                    Constants.sharedPrefrencesConstant.DEVICE_TOKEN, ""
+                )
+
+        getInactiveOffers()
 
         //setUpActiveOffersRv()
 
         return view
     }
 
-    private fun setUpActiveOffersRv() {
+    private fun getInactiveOffers() {
+
+        if(token != null){
+
+            activeOffersViewModel.getActiveOffers(this, token.toString(), loadingInactive).observe(viewLifecycleOwner){
+
+                if (it != null){
+
+                    if (it.getSuccess()!!){
+
+                        clearArrayLists()
+
+                        for (i in it.getData()!!.ListcouponResponce!!.indices) {
+
+                            val dateFormat: DateFormat =
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                    SimpleDateFormat(
+                                        "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+                                        Locale.getDefault()
+                                    )
+                                } else {
+                                    TODO("VERSION.SDK_INT < N")
+                                }
+                            val date =
+                                dateFormat.parse(it.getData()!!.ListcouponResponce!![i].stopAt.toString()) //You will get date object relative to server/client timezone wherever it is parsed
+
+                            val date1 =
+                                dateFormat.parse(it.getData()!!.ListcouponResponce!![i].startAt.toString())
+
+                            val formatter: DateFormat =
+                                SimpleDateFormat("dd-MM-yyyy") //If you need time just put specific format for time like 'HH:mm:ss'
+
+                            val dateStr = formatter.format(date)
+
+                            val startDate = formatter.format(date1)
+
+                            val valid_until = dateStr
+                            val sdf = SimpleDateFormat("dd-MM-yyyy")
+                            var strDate: Date? = null
+                            try {
+                                strDate = sdf.parse(valid_until)
+                            } catch (e: ParseException) {
+                                e.printStackTrace()
+                            }
+
+                            if (it.getData()!!.ListcouponResponce!![i].status == false) {
+
+                                Log.d("time", "stopgreater")
+
+                                offersId.add(it.getData()!!.ListcouponResponce!![i].id.toString())
+                                offersName.add(it.getData()!!.ListcouponResponce!![i].name.toString())
+                                offersDes.add(it.getData()!!.ListcouponResponce!![i].desc.toString())
+                                offersCode.add(it.getData()!!.ListcouponResponce!![i].code.toString())
+                                offersTitle.add(it.getData()!!.ListcouponResponce!![i].title.toString())
+                                offersCalcType.add(it.getData()!!.ListcouponResponce!![i].calculateType.toString())
+                                offersImg.add(it.getData()!!.ListcouponResponce!![i].bannerImage.toString())
+                                offersMin.add(it.getData()!!.ListcouponResponce!![i].minimumAmount.toString())
+                                offersUpTo.add(it.getData()!!.ListcouponResponce!![i].uptoDiscount.toString())
+                                offersStartAt.add(startDate)
+                                offersStopAt.add(dateStr)
+
+                            } else {
+
+                                Log.d("time121", "todaygreater")
+                            }
+                        }
+
+                        loadingInactive.visibility = View.GONE
+                        setUpInActiveOffersRv()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun clearArrayLists() {
 
         recycleView_models.clear()
+        offersCode.clear()
+        offersId.clear()
+        offersName.clear()
+        offersMin.clear()
+        offersTitle.clear()
+        offersCalcType.clear()
+        offersDes.clear()
+        offersImg.clear()
+        offersStartAt.clear()
+        offersStopAt.clear()
+        offersUpTo.clear()
+    }
 
-        recycleView_models.add(RecycleModel("40%","sadads", "Porridge", "Flat discount on every purchase", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Sauteed Kale", "For first time user only", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Porridge", "Flat discount on every purchase", "Offer available only 21 to 23 April"))
-        recycleView_models.add(RecycleModel("40%","sadads", "Sauteed Kale", "For first time user only", "Offer available only 21 to 23 April"))
+    private fun setUpInActiveOffersRv() {
 
-        recycleView_adapter_O = RecycleView_O(activity, recycleView_models)
-        recyclerView.adapter = recycleView_adapter_O
-        recycleView_adapter_O.notifyDataSetChanged()
+        for (i in offersId.indices) {
+
+            recycleView_models.add(
+                RecycleModel(
+                    offersId[i],
+                    offersName[i],
+                    offersTitle[i],
+                    offersCode[i],
+                    offersDes[i],
+                    offersCalcType[i],
+                    offersUpTo[i],
+                    offersMin[i],
+                    offersStartAt[i],
+                    offersStopAt[i],
+                    offersImg[i]
+                )
+            )
+
+            recycleView_adapter_O = RecycleView_O(activity, recycleView_models)
+            recyclerView.adapter = recycleView_adapter_O
+            recycleView_adapter_O.notifyItemInserted(i)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
